80febfcf58109aefcab2286ab4fe9b955e3c363e
Douglas Mitchell <roxonus@gmail.com>
2020-09-01 21:10:10 -0700

    Ran tests and fixed mistakes.

--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -3,15 +3,15 @@ const Employee = require("./Employee")
 
 class Engineer extends Employee {
     constructor(name, id, email, github) {
-        super(name, id, email, github); 
+        super(name, id, email); 
             this.github = github;
-    }
+        }
         getGithub() {
             return this.github;
         }
         getRole() {
             return "Engineer";
         }
-    }
+}
 
-module.export = Engineer;
+module.exports = Engineer;
